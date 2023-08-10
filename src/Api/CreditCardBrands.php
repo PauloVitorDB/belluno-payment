14640b549ca532feae53b740f402017e7fb42c28
Paulo <paulovitorl@Outlook.com>
2023-08-10 15:22:58 -0300

    fix

---
+++ src/Api/CreditCardBrands.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BellunoApi\Api;
+
+abstract class CreditCardBrands {
+
+    const BRANDS_NUMBERS = [
+        "MASTERCARD" => 1, 
+        "VISA" => 2, 
+        "ELO" => 3, 
+        "HIPERCARD" => 4, 
+        "CABAL" => 5, 
+        "HIPER" => 6, 
+        "AMEX" => 7
+    ];
+
+
+
+}
