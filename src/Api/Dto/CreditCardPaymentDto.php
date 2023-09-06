f9aea46cf9dad16efad5051a5ae180235fd3ba4f
Paulo <paulovitorl@Outlook.com>
2023-09-06 11:09:18 -0300

    fix: campos que podem vir nulos dtos

--- src/Api/Dto/CreditCardPaymentDto.php
+++ src/Api/Dto/CreditCardPaymentDto.php
@@ -9,28 +9,28 @@ class CreditCardPaymentDto implements JsonSerializable {
     /**
      * @todo Write general description for this property
      * @required
-     * @var string $type public property
+     * @var string|null $type public property
      */
     public $type;
 
     /**
      * @todo Write general description for this property
      * @required
-     * @var string $card public property
+     * @var string|null $card public property
      */
     public $card;
 
     /**
      * @todo Write general description for this property
      * @required
-     * @var string $brand public property
+     * @var string|null $brand public property
      */
     public $brand;
 
     /**
      * @todo Write general description for this property
      * @required
-     * @var string $installments_number public property
+     * @var string|null $installments_number public property
      */
     public $installments_number;
 
@@ -38,7 +38,7 @@ class CreditCardPaymentDto implements JsonSerializable {
     /**
      * @todo Write general description for this property
      * @required
-     * @var string $cod_aut public property
+     * @var string|null $cod_aut public property
      */
     public $cod_aut;
 
@@ -46,21 +46,21 @@ class CreditCardPaymentDto implements JsonSerializable {
     /**
      * @todo Write general description for this property
      * @required
-     * @var string $nsu_payment public property
+     * @var string|null $nsu_payment public property
      */
     public $nsu_payment;
    
     /**
      * @todo Write general description for this property
      * @required
-     * @var \DateTime $paid_at public property
+     * @var \DateTime|null $paid_at public property
     */
     public $paid_at;
 
     /**
      * @todo Write general description for this property
      * @required
-     * @var \BellunoApi\Api\Dto\CreditCardHolderDto $cardholder public property
+     * @var \BellunoApi\Api\Dto\CreditCardHolderDto|null $cardholder public property
      */
     public $cardholder;
 
@@ -68,14 +68,14 @@ class CreditCardPaymentDto implements JsonSerializable {
       /**
      * @todo Write general description for this property
      * @required
-     * @var \BellunoApi\Api\Dto\ShippingDto $billing $billing public property
+     * @var \BellunoApi\Api\Dto\ShippingDto|null $billing $billing public property
      */
     public $billing;
 
     /**
      * @todo Write general description for this property
      * @required
-     * @var \BellunoApi\Api\Dto\RefundsDto $refunds public property
+     * @var \BellunoApi\Api\Dto\RefundsDto|null $refunds public property
      */
     public $refunds;
 
